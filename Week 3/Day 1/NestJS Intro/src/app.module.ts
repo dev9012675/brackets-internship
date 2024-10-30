3ea69fb602039327f9eee8fa613884471f5a0a29
dev9012675 <danialcs606@gmail.com>
2024-10-30 20:45:27 +0500

    Renamed task folders and modified Task 1

--- Week 3/Day 1/NestJS Intro/src/app.module.ts
+++ Week 3/Day 1/NestJS Intro/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloController } from './hello/hello.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, HelloController],
+  providers: [AppService]
 })
 export class AppModule {}
